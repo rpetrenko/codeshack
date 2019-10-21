5cda670d7fe92864d005e2574bbf940c26e9cabc
Roman Petrenko <rpetrenko@gmail.com>
2019-10-20 18:26:49 -0700

    add controll to print dataframe

--- other/loan_calculator.py
+++ other/loan_calculator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def loan_montly(payment, balance, interest, monthly_pay, months, data):
+def loan_monthly(payment, balance, interest, monthly_pay, months, data):
     if payment == 0:
         payment_paid = 0
     else:
@@ -15,7 +15,7 @@ def loan_montly(payment, balance, interest, monthly_pay, months, data):
         if payment + 1 == months:
             monthly_pay += new_balance
             new_balance = 0
-        loan_montly(payment + 1, new_balance, interest, monthly_pay, months, data)
+        loan_monthly(payment + 1, new_balance, interest, monthly_pay, months, data)
 
 
 if __name__ == "__main__":
@@ -24,11 +24,13 @@ if __name__ == "__main__":
     since within the last month the interest is getting accured
     """
     data = []
-    amount, annual_interest, monthly_pay, years = 150000, 0.06, 899.33, 30
+    amount, annual_interest, monthly_pay, years = 750000, 0.0373, 3464.86, 30
+    # amount, annual_interest, monthly_pay, years = 150000, 0.06, 899.33, 30
     # amount, annual_interest, monthly_pay, years = 2000, 0.09, 91.37, 2
     months = years * 12
-    loan_montly(0, amount, annual_interest / 12., monthly_pay, months, data)
+    loan_monthly(0, amount, annual_interest / 12., monthly_pay, months, data)
     df = pd.DataFrame(data).set_index('month')
-    print(df[["payment", "balance"]])
+    with pd.option_context('display.max_rows', 24, 'display.max_columns', None):
+        print(df[["payment", "balance"]])
     assert df.shape[0] == months + 1, "something's wrong with the number of payments"
     assert df.loc[months]['balance'] == 0, "final balance is not zero"
